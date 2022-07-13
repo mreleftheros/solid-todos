231f398f3fc0c122a4be75e5b7214759670abcac
eleftheros <eleftheros_all@protonmail.com>
2022-07-13 14:30:04 +0300

    Add todoStore

--- src/store/todos.js
+++ src/store/todos.js
@@ -1,5 +1,27 @@
-import {createStore} from 'solid-js/store'
+import { createStore } from 'solid-js/store';
 
 export const createTodoStore = () => {
-  createStore([])
-}
+  const [todos, setTodos] = createStore([]);
+  const len = () => todos.length;
+  const id = () => (len() === 0 ? 0 : Math.max(...todos.map(t => t.id)) + 1);
+
+  const addTodo = text =>
+    setTodos(prev => [...prev, { id, text, done: false }]);
+
+  const deleteTodo = id => setTodos(prev => prev.filter(t => t.id !== id));
+
+  const toggleTodo = id =>
+    setTodos(
+      t => t.id === id,
+      'done',
+      done => !done
+    );
+
+  return {
+    todos,
+    len: len(),
+    addTodo,
+    deleteTodo,
+    toggleTodo,
+  };
+};
